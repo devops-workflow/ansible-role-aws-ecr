5cfc372a12810ca5e730d8adbb4d34ee029a1740
Steven Nemetz <snemetz@wiser.com>
2017-10-05 13:42:39 -0700

    Debug test

--- tests/test.sh
+++ tests/test.sh
@@ -3,6 +3,7 @@ export ANSIBLE_ROLES_PATH=$(pwd)/roles:${ANSIBLE_ROLES_PATH}
 #/etc/ansible/roles
 
 # Setup and run role
-ansible-galaxy install -r requirements.yml
-# --roles-path $(pwd)/roles
+ansible-galaxy install -f -r requirements.yml
+### Test: syntax
+ansible-playbook -i inventory test.yml --syntax-check
 ansible-playbook -i inventory test.yml
