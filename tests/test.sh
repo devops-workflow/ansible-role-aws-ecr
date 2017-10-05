fc8856ffba5218b12b75f72ac0430e047f1951f3
Steven Nemetz <snemetz@wiser.com>
2017-10-05 13:48:27 -0700

    Add AWS region

--- tests/test.sh
+++ tests/test.sh
@@ -6,4 +6,4 @@ export ANSIBLE_ROLES_PATH=$(pwd)/roles:${ANSIBLE_ROLES_PATH}
 ansible-galaxy install -f -r requirements.yml
 ### Test: syntax
 ansible-playbook -i inventory test.yml --syntax-check
-ansible-playbook -i inventory test.yml
+ansible-playbook -i inventory test.yml --extra-vars aws_region=us-west-2
