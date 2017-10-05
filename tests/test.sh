94bf687040f6a0117c8b5f5d4151c20cd31b1464
Steven Nemetz <snemetz@wiser.com>
2017-10-05 12:48:35 -0700

    Change role path

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,8 @@
 
-ANSIBLE_ROLES_PATH=$(pwd)/roles:${ANSIBLE_ROLES_PATH}
+export ANSIBLE_ROLES_PATH=$(pwd)/roles:${ANSIBLE_ROLES_PATH}
 #/etc/ansible/roles
 
 # Setup and run role
 ansible-galaxy install -r requirements.yml
+# --roles-path $(pwd)/roles
 ansible-playbook -i inventory test.yml
