64a1266eb26865db045fa18dd6e48d1c3c34d02a
Steven Nemetz <snemetz@wiser.com>
2017-10-05 14:39:20 -0700

    Debug test

--- tests/test.sh
+++ tests/test.sh
@@ -6,6 +6,8 @@ echo "Installing roles..."
 ansible-galaxy install -f -r requirements.yml
 ### Test: syntax
 echo "Test: syntax"
-ansible-playbook -i inventory test.yml --syntax-check
+ansible-playbook -i inventory test.yml --syntax-check --list-tasks
+#echo "Test: Lint"
+#ansible-lint -x ANSIBLE0012,ANSIBLE0013 test.yml
 echo "Run test playbook"
 ansible-playbook -i inventory test.yml --extra-vars aws_region=us-west-2
