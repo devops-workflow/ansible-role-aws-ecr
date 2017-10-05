797bd0458904c1825b01232e012e58e61d967331
Steven Nemetz <snemetz@wiser.com>
2017-10-05 15:35:01 -0700

    Set python

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,12 @@
 
 export ANSIBLE_ROLES_PATH=$(pwd)/roles:/etc/ansible/roles
+python=$(which python)
 
 env | sort | grep -v ^LESS_TERMCAP
+echo "Install: pip modules..."
+pip install -r requirements.txt
 ansible --version
-echo "Installing roles..."
+echo "Install: Ansible roles..."
 ansible-galaxy install -f -r requirements.yml
 ### Test: syntax
 echo "Test: syntax"
@@ -11,4 +14,4 @@ ansible-playbook -i inventory test.yml --syntax-check --list-tasks
 #echo "Test: Lint"
 #ansible-lint -x ANSIBLE0012,ANSIBLE0013 test.yml
 echo "Run test playbook"
-ansible-playbook -i inventory test.yml --extra-vars aws_region=us-west-2 -vvvv
+ansible-playbook -i inventory test.yml --extra-vars ansible_python_interpreter=${python} --extra-vars aws_region=us-west-2 -vvvv
