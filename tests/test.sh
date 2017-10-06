01c0ab5fbd3a7db5bccddcc64063af949451c23f
Steven Nemetz <snemetz@wiser.com>
2017-10-06 10:22:46 -0700

    Update to match playbook repo

--- tests/test.sh
+++ tests/test.sh
@@ -3,17 +3,21 @@ export ANSIBLE_ROLES_PATH=$(pwd)/roles:/etc/ansible/roles
 python=$(which python)
 
 env | sort | grep -v ^LESS_TERMCAP
-echo "Install: pip modules..."
+
+echo -e "\nInstall: pip modules..."
 pip install -r requirements.txt
-echo "Versions"
+
+echo -e "\nVersions"
 ansible --version
 docker --version
-echo "Install: Ansible roles..."
+
+echo -e "\nInstall: Ansible roles..."
 ansible-galaxy install -f -r requirements.yml
+
 ### Test: syntax
 echo "Test: syntax"
 ansible-playbook -i inventory test.yml --syntax-check --list-tasks
-#echo "Test: Lint"
-#ansible-lint -x ANSIBLE0012,ANSIBLE0013 test.yml
+echo "Test: Lint"
+ansible-lint -x ANSIBLE0012,ANSIBLE0013 test.yml
 echo "Run test playbook"
 ansible-playbook -i inventory test.yml --extra-vars ansible_python_interpreter=${python} --extra-vars aws_region=us-west-2 -vvvv
